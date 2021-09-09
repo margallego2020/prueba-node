0384250874d91771a1b7b1e05dd64fe9f442a7ff
margallego2020 <martingallego_ar@yahoo.com.ar>
2021-09-08 23:42:17 -0300

    visto css y html en intro-html

--- INTRO-HTML/app.js
+++ INTRO-HTML/app.js
@@ -3,14 +3,14 @@ const app = express();
 const path = require ('path');
 
 
-const publicPath = path.join(__dirname, './public');
+const publicFolderPath = path.resolve(__dirname, './public');
 
-app.use( express.static(publicPath) );
+app.use( express.static(publicFolderPath) );
 
 app.listen(3000, () => {
     console.log("Servidor corriendo en el puerto: 3000", "http://localhost:3000" );
 })
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'./views/home.html'));
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
 })
