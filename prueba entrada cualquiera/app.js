0a3d10f1c1078c56c2bcc39785d5c2fe90bd2323
margallego2020 <martingallego_ar@yahoo.com.ar>
2021-09-22 20:29:25 -0300

    tomando todo

---
+++ prueba entrada cualquiera/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require ('path');
+
+
+const publicFolderPath = path.resolve(__dirname, './public');
+
+app.use( express.static(publicFolderPath) );
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo en el puerto: 3000", "http://localhost:3000" );
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+})
